914b90afa48b1130b67982d51e14cd54e4e63bbc
sjh990519 <sjh990519@gmail.com>
2022-07-25 21:14:13 +0900

    KOBUKI

---
+++ kobuki_ws/devel/share/gennodejs/ros/map_msgs/srv/_index.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+let GetMapROI = require('./GetMapROI.js')
+let SaveMap = require('./SaveMap.js')
+let ProjectedMapsInfo = require('./ProjectedMapsInfo.js')
+let GetPointMap = require('./GetPointMap.js')
+let SetMapProjections = require('./SetMapProjections.js')
+let GetPointMapROI = require('./GetPointMapROI.js')
+
+module.exports = {
+  GetMapROI: GetMapROI,
+  SaveMap: SaveMap,
+  ProjectedMapsInfo: ProjectedMapsInfo,
+  GetPointMap: GetPointMap,
+  SetMapProjections: SetMapProjections,
+  GetPointMapROI: GetPointMapROI,
+};
---
+++ kobuki_ws/devel/share/gennodejs/ros/move_base_msgs/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let RecoveryStatus = require('./RecoveryStatus.js');
+let MoveBaseActionGoal = require('./MoveBaseActionGoal.js');
+let MoveBaseAction = require('./MoveBaseAction.js');
+let MoveBaseResult = require('./MoveBaseResult.js');
+let MoveBaseActionFeedback = require('./MoveBaseActionFeedback.js');
+let MoveBaseFeedback = require('./MoveBaseFeedback.js');
+let MoveBaseActionResult = require('./MoveBaseActionResult.js');
+let MoveBaseGoal = require('./MoveBaseGoal.js');
+
+module.exports = {
+  RecoveryStatus: RecoveryStatus,
+  MoveBaseActionGoal: MoveBaseActionGoal,
+  MoveBaseAction: MoveBaseAction,
+  MoveBaseResult: MoveBaseResult,
+  MoveBaseActionFeedback: MoveBaseActionFeedback,
+  MoveBaseFeedback: MoveBaseFeedback,
+  MoveBaseActionResult: MoveBaseActionResult,
+  MoveBaseGoal: MoveBaseGoal,
+};
